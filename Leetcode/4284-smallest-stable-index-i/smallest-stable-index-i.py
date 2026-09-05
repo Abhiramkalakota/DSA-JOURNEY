bad32cc4860b32bd37165fc1bf9abccf6fd8fd02
Abhiramkalakota <abhiramkalakota@gmail.com>
2026-09-05 16:00:54 +0530

    Time: 17 ms (30.86%) | Memory: 12.2 MB (100.00%) - LeetSync

---
+++ Leetcode/4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        new_l=[]
+        for i in range(len(nums)):
+            new_l.append(nums[i])
+            max1=max(new_l)
+            min1=min(nums[i:len(nums)])
+            ans=max1-min1
+            if ans<=k:
+                return i
+        return -1
